4bd2349e79e60569772d8429646ead754f06b2ef
Adelene LAI <adelene.lai@mp0404.fritz.box>
2022-07-11 16:24:36 +0200

    change module, pytest now works

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,12 +1,12 @@
 import pytest
-from classify_homologues.utils import *
+from src.classify_homologues.utils import *
 
 
 def test_parser() -> None:
     """
     Test to check arguments are correctly specified.
     """
-    assert 1 = 1
+    assert 1 == 1
 
 def test_read_smiles_csv() -> None:
     """
@@ -18,7 +18,7 @@ def test_read_smiles_csv() -> None:
     assert len_mols >= 2 , "Not enough molecules. Check validity of SMILES input or add more SMILES."
 
 
-def test_read_labels_csv(len_mols) -> None:
+def test_read_labels_csv() -> None:
     """
     Test to check every molecule has a name i.e. one label per Mol.
     """
